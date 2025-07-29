e112e949672a9138c99dc527e454ad57affaa601
Viktor <viktor.avgn@yandex.ru>
2025-07-30 01:12:26 +0700

    Верстка табов (вкладок), логика

--- tabs/main.js
+++ tabs/main.js
@@ -1,1 +1,27 @@
 'use strict'
+
+const buttons = document.querySelector('.buttons')
+const links = document.querySelectorAll('.link')
+const block = document.querySelectorAll('.block')
+
+buttons.addEventListener('click', function (e) {
+  if (e.target === buttons) {
+    return
+  }
+  links.forEach((el) => {
+    el.classList.remove('link-active')
+  })
+
+  block.forEach((item) => {
+    item.classList.add('hidden')
+  })
+
+  if (e.target.classList.contains('link')) {
+    e.target.classList.add('link-active')
+  }
+
+  const data = e.target.getAttribute('data-')
+  console.log(data)
+
+  document.querySelector(`.block-content--${data}`).classList.remove('hidden')
+})
