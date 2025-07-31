93cf0f64efbce6d46d188ca9585ccc9cbac90cf1
Viktor <viktor.avgn@yandex.ru>
2025-08-01 00:04:29 +0700

    Скрипт под анимацию хедера

--- header_animation/main.js
+++ header_animation/main.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const header = document.querySelector('header')
+const links = document.querySelectorAll('.link')
+const logo = document.querySelector('.logo')
+
+function linksAnimation(e) {
+  if (e.target.classList.contains('link')) {
+    const targetLink = e.target
+
+    links.forEach((el) => {
+      el.style.opacity = this
+    })
+    logo.style.opacity = this
+    header.classList.toggle('head')
+    targetLink.style.opacity = 1
+  }
+}
+
+header.addEventListener('mouseover', linksAnimation.bind(0.4))
+
+header.addEventListener('mouseout', linksAnimation.bind(1))
