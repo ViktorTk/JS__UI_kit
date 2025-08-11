df7b0ee9351b84d2802a7a4ce83e4eee1b6a84d9
Viktor <viktor.avgn@yandex.ru>
2025-08-12 00:06:22 +0700

    аккордион-меню, логика

--- accordion_menu/main.js
+++ accordion_menu/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const accordionItems = document.querySelectorAll('.accordion-item')
+
+accordionItems.forEach((item) => {
+  const heading = item.querySelector('.accordion__block')
+  const content = item.querySelector('.accordion__content')
+  const plus = item.querySelector('.plus')
+
+  heading.addEventListener('click', () => {
+    content.classList.toggle('active')
+    heading.classList.toggle('active')
+    plus.classList.toggle('rotate')
+  })
+})
