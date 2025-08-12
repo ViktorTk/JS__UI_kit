4f864d6184cb520874e17d3f1b8a45d15414bdf6
Viktor <viktor.avgn@yandex.ru>
2025-08-13 00:21:34 +0700

    Переключение темы страницы (темная / светлая); дополнил сохранение состояния темы в локальном хранилище

--- switch_theme/main.js
+++ switch_theme/main.js
@@ -1,8 +1,25 @@
 'use strict'
 
+// // без сохранения состояния темы в локальном хранилище
+
+// const btn = document.querySelector('.btn')
+
+// btn.addEventListener('click', () => {
+//   const body = document.body
+//   body.classList.toggle('dark-mode')
+// })
+
+// // с сохранением состояния темы страницы
+
 const btn = document.querySelector('.btn')
+const body = document.body
+const save = localStorage.getItem('theme')
+
+if (save) body.classList.add(save)
 
 btn.addEventListener('click', () => {
-  const body = document.body
+  const a1 = body.classList.contains('dark-mode') ? 'light-theme' : 'dark-mode'
+
   body.classList.toggle('dark-mode')
+  localStorage.setItem('theme', a1)
 })
