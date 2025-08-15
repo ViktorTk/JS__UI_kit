c3854b8ede7e13e045ede8da59b77e2032445ee0
Viktor <viktor.avgn@yandex.ru>
2025-08-16 04:02:06 +0700

    Комментарии и отзывы; добавил логику на рейтинг звезд

--- reviews_and_comments/main.js
+++ reviews_and_comments/main.js
@@ -1,1 +1,28 @@
 'use strict'
+
+// rating star
+const stars = document.querySelectorAll('i')
+const spanNumberRating = document.querySelector('.spanNumberRating')
+//
+const btnComment = document.querySelector('.add-comment')
+const form = document.querySelector('.form')
+const btnCloseForm = document.querySelector('.close')
+const btnSendForm = document.querySelector('.send')
+const formInputName = document.querySelector('.form__input-name')
+const formInputLastName = document.querySelector('.form__input-surname')
+const wrapper = document.querySelector('.wrapper')
+const textArea = document.querySelector('.form__textarea')
+const commentUser = document.querySelector('.block__comment')
+
+stars.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    spanNumberRating.textContent = index + 1
+    stars.forEach((item, index2) => {
+      index >= index2
+        ? item.classList.add('active')
+        : item.classList.remove('active')
+    })
+  })
+})
+
+//
